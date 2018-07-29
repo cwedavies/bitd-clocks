c223d06acc9eeb7d3bfb16420ded231bd79e02ff
Cameron W. E. Davies <cwedavies@gmail.com>
2018-07-29 13:39:54 -0400

    clean up server

--- server/lib/server.js
+++ server/lib/server.js
@@ -9,20 +9,14 @@ export default function server(log) {
   const server = http.createServer();
   const io = socketio(server, { path: '/api', serveClient: false });
 
+
   io.on('connection', socket => {
+    const emitUpdate = _.compose([emitAction(socket), clockUpdate]);
+
     log.info(`connection ${socket.id}`);
 
-    store.changes(_.compose([emitAction(socket), clockUpdate]));
-    //socket.emit('action', { type: 'clock/UPDATE', ticks: 2 });
-    socket.on('action', ({type}) => {
-      if (type === 'clock/TICKDOWN') {
-        store.tickdown();
-      }
-
-      if (type === 'clock/TICKUP') {
-        store.tickup();
-      }
-    });
+    store.changes(emitUpdate);
+    socket.on('action', handleAction(store));
   });
 
   return server;
@@ -36,4 +30,16 @@ function emitAction(socket) {
   return action => {
     socket.emit('action', action);
   };
+}
+
+function handleAction(store) {
+  return ({ type }) => {
+    if (type === 'clock/TICKDOWN') {
+      store.tickdown();
+    }
+
+    if (type === 'clock/TICKUP') {
+      store.tickup();
+    }
+  }
 }
